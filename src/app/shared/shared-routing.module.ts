b62e7ad64a3ed194288a3005bc7dfc3818b9dca5
yu728 <yuu.watanabe0728@gmail.com>
2020-04-14 15:26:29 +0900

    feat(shared): footerComponent作成#5

--- src/app/shared/shared-routing.module.ts
+++ src/app/shared/shared-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    component:
+    component: FooterComponent
   }
 ];
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FooterComponent],
   imports: [
     CommonModule,
     SharedRoutingModule
