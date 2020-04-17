331953588f171880bcf25330675e2061446a17b6
yu728 <yuu.watanabe0728@gmail.com>
2020-04-17 11:08:28 +0900

    feat(speech): speechComponentの作成#28

---
+++ src/app/chat/speech/speech.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-speech',
+  templateUrl: './speech.component.html',
+  styleUrls: ['./speech.component.scss']
+})
+export class SpeechComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
