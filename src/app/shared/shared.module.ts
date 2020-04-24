24f1c2371247d8e21c5aefd8eb0dc6267e989266
yu728 <yuu.watanabe0728@gmail.com>
2020-04-24 09:41:21 +0900

    fix(shared): 不要なimports削除 #fix49

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { SharedRoutingModule } from './shared-routing.module';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    SharedRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
+  imports: [],
   exports: [FormsModule, ReactiveFormsModule],
 })
 export class SharedModule {}
