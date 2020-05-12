3ba73238e14cef8c47f3a516323dc4cd208ad490
yu728 <yuu.watanabe0728@gmail.com>
2020-05-12 15:43:32 +0900

    feat(mypage): latest-postにデータを渡す

--- src/app/mypage/mypage/mypage.component.ts
+++ src/app/mypage/mypage/mypage.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from 'src/app/services/chat.service';
 
 @Component({
   selector: 'app-mypage',
   templateUrl: './mypage.component.html',
-  styleUrls: ['./mypage.component.scss']
+  styleUrls: ['./mypage.component.scss'],
 })
 export class MypageComponent implements OnInit {
+  uid = this.chatService.uid;
+  posts$ = this.chatService.getAllChat(this.uid);
 
-  constructor() { }
+  constructor(private chatService: ChatService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
