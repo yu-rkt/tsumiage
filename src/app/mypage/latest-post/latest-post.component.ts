0f98e726c66c3b7054a12827fc7f1b59e2e68ffe
yu728 <yuu.watanabe0728@gmail.com>
2020-05-12 14:11:16 +0900

    feat(mypage): mypage画面のレイアウト追加

---
+++ src/app/mypage/latest-post/latest-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Message } from 'src/app/interfaces/message';
+
+@Component({
+  selector: 'app-latest-post',
+  templateUrl: './latest-post.component.html',
+  styleUrls: ['./latest-post.component.scss'],
+})
+export class LatestPostComponent implements OnInit {
+  @Input() latestPost: Message;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
