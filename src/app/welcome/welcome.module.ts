bfa19a3d41b29a654a5e651d09650bce87eb33d4
yu728 <yuu.watanabe0728@gmail.com>
2020-05-14 10:07:49 +0900

    fwat(welcome): welcome画面実装

--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -5,9 +5,11 @@ import { WelcomeRoutingModule } from './welcome-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 import { MatButtonModule } from '@angular/material/button';
+import { TopContentComponent } from './top-content/top-content.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  declarations: [WelcomeComponent],
+  declarations: [WelcomeComponent, TopContentComponent, ButtonComponent],
   imports: [CommonModule, WelcomeRoutingModule, MatButtonModule],
 })
 export class WelcomeModule {}
